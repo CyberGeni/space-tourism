93b85c0023e3933e9a5c1750e8903204a056696f
CyberGeni <70205370+CyberGeni@users.noreply.github.com>
2022-03-09 13:45:04 +0100

    router setup

--- src/main.js
+++ src/main.js
@@ -1,7 +1,31 @@
+// necessary importing of files
+
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+
+// CSS Library
+
+import 'animate.css';
+ 
+// Router imports
+
+
+// Router init
+
+Vue.use(VueRouter);
+const router = new VueRouter({
+  routes : [
+    // essential routing of components  
+    // {path: '/', component: Home}
+  ],
+  mode : 'history'
+})
+
+// Vue init
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  router: router,
 })
