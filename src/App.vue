af9d3ac9e77c417d91f1e30dee73cc635299107f
CyberGeni <70205370+CyberGeni@users.noreply.github.com>
2022-03-09 13:22:08 +0100

    project setup

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1>in progress</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
