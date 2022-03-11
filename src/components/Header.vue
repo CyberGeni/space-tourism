ef15b9617710df13f025506a33acca5ba53ac629
CyberGeni <70205370+CyberGeni@users.noreply.github.com>
2022-03-11 19:21:47 +0100

    feat: perfected glassmorphic effect

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,21 +1,22 @@
 <template>
-    <header class="absolute z-100 text-white md:md:items-center w-full md:mx-auto flex md:justify-between h-fit ">
-        <img class="md:pl-6 h-fit" src="/src/assets/shared/logo.svg" alt="">
+    <header class="absolute z-100 text-white md:md:items-center w-full md:mx-auto flex md:justify-between h-fit lg:mt-12">
+        <img class="md:pl-6 lg:pl-12 h-fit" src="/src/assets/shared/logo.svg" alt="">
+        <div class="hidden absolute z-100 lg:ml-36 lg:flex lg:w-[50%] lg:mx-auto lg:h-[1px] opacity-30 lg:bg-white"></div>
         <nav class="md:w-3/4 md:flex md:justify-end" id="">
             <img class="hamburger md:hidden h-auto w-auto" src="/src/assets/shared/icon-hamburger.svg" alt="">
-            <ul class="menu md:flex md:h-20 bg-white md:items-center md:space-x-8">
+            <ul class="menu tracking-wide text-lg font-sub-heading md:flex md:h-20 bg-white md:items-center md:space-x-8 lg:h-18 lg:space-x-12">
                 <img class="md:hidden" src="/src/assets/shared/icon-close.svg" alt="">
                 <li>
-                    <router-link to="/">00 Home</router-link>
+                    <router-link to="/"><span class="font-bold mr-2">00</span>  Home</router-link>
                 </li>
                 <li>
-                    <router-link to="/destination">01 Destination</router-link>
+                    <router-link to="/destination"> <span class="font-bold mr-2">01</span>Destination</router-link>
                 </li>
                 <li>
-                    <router-link to="/crew">02 Crew</router-link>
+                    <router-link to="/crew"> <span class="font-bold mr-2">02</span>Crew</router-link>
                 </li>
                 <li class="pr-8">
-                    <router-link to="/technologies">03 Technology</router-link>
+                    <router-link to="/technologies"> <span class="font-bold mr-2">03</span>Technology</router-link>
                 </li>
             </ul>
         </nav> 
@@ -30,8 +31,8 @@ export default {
 </script>
 <style scoped>
 nav ul {
-    background-color: rgb(255, 255, 255, 0.4);
-    backdrop-filter: blur(81.5px);
-    -webkit-backdrop-filter: blur(81.5px);
+    background-color: rgba(255, 255, 255, 0.15);
+    backdrop-filter: blur(16px);
+    -webkit-backdrop-filter: blur(16px);
 }
 </style>
