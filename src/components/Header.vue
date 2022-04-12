868189e2e42dbda69379b67977a70787413a4b41
CyberGeni <70205370+CyberGeni@users.noreply.github.com>
2022-04-12 03:38:01 +0100

    navbar close after clicking link

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,22 +1,22 @@
 <template>
-    <header class="absolute z-100 text-white flex justify-between items-center md:px-0 md:py-0 md:md:items-center w-full md:mx-auto flex md:justify-between h-fit lg:mt-12 ">
+    <header class="absolute z-1000 text-white flex justify-between items-center md:px-0 md:py-0 md:md:items-center w-full md:mx-auto flex md:justify-between h-fit lg:mt-12 ">
         <img class="absolute px-8 mt-32 md:mt-24 md:pl-6 lg:pl-12 h-fit" src="/src/assets/shared/logo.svg" alt="">
         <div class="hidden absolute z-100 lg:mt-20 lg:ml-36 lg:flex lg:w-[50%] lg:mx-auto lg:h-[1px] opacity-30 lg:bg-white"></div>
         <nav class="md:w-3/4 md:flex md:justify-end" id="">
             <a @click="toggle" class="icon hover:cursor-pointer">
                 <img class="absolute right-8 top-12 md:hidden h-auto w-auto" src="/src/assets/shared/icon-hamburger.svg" alt="">
             </a>
-            <ul :class="[isActive ? 'block animate__animated animate__bounceInRight':'hidden animate__animated animate__bounceOutRight']" class="absolute right-0 space-y-8 w-2/3 pt-12 md:pt-0 md:space-y-0 px-8 h-screen md:w-fit uppercase tracking-wider text-lg font-sub-heading md:flex md:h-20 bg-white md:items-center md:space-x-8 lg:h-18 lg:space-x-12">
+            <ul :class="[isActive ? 'block animate__animated animate__bounceInRight':'hidden']" class="z-10000 absolute right-0 space-y-8 w-2/3 pt-12 md:pt-0 md:space-y-0 px-8 h-screen md:w-fit uppercase tracking-wider text-lg font-sub-heading md:flex md:h-20 bg-white md:items-center md:space-x-8 lg:h-18 lg:space-x-12">
                 <img @click="toggle" class="absolute right-8 md:hidden w-7" src="/src/assets/shared/icon-close.svg" alt="">
-                <li>
+                <li @click="isActive = !isActive">
                     <router-link class="md:hover:decoration-2 md:hover:decoration-zinc-500 md:hover:underline hover:underline-offset-[30px]" exact="" to="/"><span class="font-bold mr-2">00</span>Home</router-link>
-                <li>
+                <li @click="isActive = !isActive">
                     <router-link class="md:hover:decoration-2 md:hover:decoration-zinc-500 md:hover:underline md:hover:underline-offset-[30px]" to="/destination"> <span class="font-bold mr-2">01</span>Destination</router-link>
                 </li>
-                <li>
+                <li @click="isActive = !isActive">
                     <router-link class="md:hover:decoration-2 md:hover:decoration-zinc-500 md:hover:underline md:hover:underline-offset-[30px]" to="/crew"> <span class="font-bold mr-2">02</span>Crew</router-link>
                 </li>
-                <li class="pr-8">
+                <li @click="isActive = !isActive" class="pr-8">
                     <router-link class="md:hover:decoration-2 md:hover:decoration-zinc-500 md:hover:underline md:hover:underline-offset-[30px]" to="/technologies"> <span class="font-bold mr-2">03</span>Technology</router-link>
                 </li>
             </ul>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,11 +8,12 @@
   because we’ll give you a truly out of this world experience!</p>
         </section>
         <section class="md:flex md:justify-center">
-            <div class="rounded-full w-40 h-40 md:w-48 md:h-48 flex items-center justify-center text-2xl uppercase font-heading text-deepBlue bg-white hover:ring-gray-200 hover:ring-opacity-20 hover:ring-[36px] transition-all my-6">
-                <router-link to="/destination/moon">
+            <router-link to="/destination/moon">
+                <div class="rounded-full  w-40 h-40 md:w-48 md:h-48 flex items-center justify-center text-2xl uppercase font-heading text-deepBlue bg-white hover:ring-gray-200 hover:ring-opacity-20 hover:ring-[36px] transition-all my-6">    
                     Explore
-                </router-link>
-            </div>
+                </div>        
+            </router-link>
+            
         </section>
     </main>
 </template>
