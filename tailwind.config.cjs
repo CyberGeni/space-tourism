6c83ced1f57e178b2e14d775a2d7a1cd0249b529
Treasure Ajefu <treasureajefu@gmail.com>
2023-02-18 11:20:57 +0100

    added custom colors to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,10 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      darkBlue: "#0B0D17",
+      lavender: "#D0D6F9",
+    },
     extend: {
       fontFamily: {
         Barlow: ["Barlow Condensed", "sans-serif"],
